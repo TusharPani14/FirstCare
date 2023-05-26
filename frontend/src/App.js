60c6e274ca8e631cede066a74a91ae40e0d44b00
SamSoham <ssoham5700@gmail.com>
2023-05-26 21:24:43 +0530

    Billing page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import SignIn from "./Pages/AdminPages/SignIn";
 import LogIn from "./Pages/UserPages/Login";
 import StockList from "./Pages/AdminPages/StockList";
 import StockView from "./Pages/UserPages/StockView";
+import Bill from "./Pages/UserPages/Bill";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/adminLogin" element={<SignIn />} />
         <Route path="/userLogin" element={<LogIn />} />
         <Route path="/adminStockList" element={<StockList/>} />
+        <Route path="/billpage" element={<Bill/>} />
         <Route path="/" element={<StockView/>} />
       </Routes>
     </>
