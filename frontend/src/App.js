db094b24fd8289e2091480df6f433a41a052957c
Milan Ram <milanram0780@gmail.com>
2023-05-26 14:18:24 +0530

    feat : user stock view page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignIn from "./Pages/AdminPages/SignIn";
 import LogIn from "./Pages/UserPages/Login";
 import StockList from "./Pages/AdminPages/StockList";
+import StockView from "./Pages/UserPages/StockView";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/adminLogin" element={<SignIn />} />
         <Route path="/userLogin" element={<LogIn />} />
-        <Route path="/" element={<StockList/>} />
+        <Route path="/adminStockList" element={<StockList/>} />
+        <Route path="/" element={<StockView/>} />
       </Routes>
     </>
   );
