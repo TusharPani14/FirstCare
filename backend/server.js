f3fa8fa2881839e362a7cb6743633142703a529b
Tushar Panigrahi <tusharpanigrahi66@gmail.com>
2023-06-01 18:24:51 +0530

    development

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,27 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/user", loginRoute);
 app.use("/stock", stockRoute);
 app.use("/bill", billRoute);
+
+//----------------------------Development-----------------------------------
+
+const __dirname1 = path.resolve();
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+//----------------------------Development-----------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
