c432b6b27914267268a929e3a0af8e18762c1f2e
Tushar Panigrahi <tusharpanigrahi66@gmail.com>
2023-07-04 20:20:49 +0530

    backend change

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema(
-  {
-    email_phoneNo: { type: String, required: true },
-    password: { type: String, required: true },
-    bills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bills" }],
-    fullBill: [{ type: Object }],
-  },
-  {
-    timestamps: true,
-  }
-);
+const createUserModel = (connection) => {
+  const userSchema = mongoose.Schema(
+    {
+      email_phoneNo: { type: String, required: true },
+      password: { type: String, required: true },
+      location: {
+        type: String,
+        enum: ["Adakata", "Sorada", "All"],
+        default: "All",
+        required: true,
+      },
+      bills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bills" }],
+      fullBill: [{ type: Object }],
+    },
+    {
+      timestamps: true,
+    }
+  );
 
-userSchema.virtual("billsData", {
-  ref: "Bills",
-  localField: "bills",
-  foreignField: "_id",
-});
+  userSchema.virtual("billsData", {
+    ref: "Bills",
+    localField: "bills",
+    foreignField: "_id",
+  });
 
-userSchema.set("toObject", { virtuals: true });
-userSchema.set("toJSON", { virtuals: true });
+  userSchema.set("toObject", { virtuals: true });
+  userSchema.set("toJSON", { virtuals: true });
 
-const User = mongoose.model("User", userSchema);
+  return connection.model("User", userSchema);
+};
 
-module.exports = User;
+module.exports = createUserModel;
